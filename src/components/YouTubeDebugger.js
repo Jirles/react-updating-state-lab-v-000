03bd4adbc95c9be89cb93dd80e164ce84611e9db
Emily Jirles <emily.jirles@gmail.com>
2018-07-30 16:36:41 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -18,7 +18,7 @@ export default class YouTubeDebugger extends React.Component {
   render(
     return (
       <button className='bitrate' onClick={}>Change Bitrate</button>
-      <button className='resolution'>Change Resolution</button>
+      <button className='resolution' onClick={}>Change Resolution</button>
     )
   )
 }
