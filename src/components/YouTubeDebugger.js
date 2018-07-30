0291169983c018f474c5f7ddb5e0864ff83bb88c
Emily Jirles <emily.jirles@gmail.com>
2018-07-30 16:37:55 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -16,7 +16,7 @@ export default class YouTubeDebugger extends React.Component {
     }
   }
   updateBitrate = () => {
-
+    this.setState(settings: Object.assing({}, {bitrate: 12}, ...this.state.settings))
   }
 
   updateResolution = () => {
